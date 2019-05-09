c594175cf1ef45257c6d8df1560d9b9d9329ad87
Luis Enrique Vite Aquino <luis.vite0307@gmail.com>
2019-05-09 12:01:53 -0500

    Actualización
    Se incluye proyecto base, ademas de añadir el proyecto sobre el cual se estara trabajando

---
+++ Proyecto Final/PFCG/figuras.h
@@ -0,0 +1,27 @@
+//#include <windows.h>  //Solo para Windows
+//#include <GL/gl.h>     // The GL Header File
+//#include <stdio.h>
+
+//#include <math.h>
+#include "Main.h"
+
+
+class CFiguras
+{
+	public:
+
+	float text_der;
+	float text_izq;
+
+	void esfera(GLfloat radio, int meridianos, int paralelos, GLuint text); //Func�on creacion esfera
+	void cilindro(float radio, float altura, int resolucion, GLuint text);	//Func�on creacion cilindro
+	void cono(float altura, float radio, int resolucion, GLuint text);		//Func�on creacion cono
+	void prisma_anun (GLuint text, GLuint text2);							//Func�on creacion movimiento
+	void prisma (float altura, float largo, float profundidad, GLuint text);//Func�on creacion prisma
+	void prisma2 (GLuint text, GLuint text2);
+	void skybox(float altura, float largo, float profundidad, GLuint text, GLuint text1);	//Funcion creacion cielo
+	void skybox2 (float altura, float largo, float profundidad, GLuint text);
+	void torus(GLfloat radioM, GLfloat radiom, int meridianos, int paralelos);
+
+
+};
---
+++ Proyecto Final/PFCG/main.cpp
@@ -0,0 +1,170 @@
+//Semestre 2019 - 2
+//************************************************************//
+//************************************************************//
+//************** Alumno (s): *********************************//
+//*************		Luis Enrique Vite Aquino			******//
+//*************											******//
+//************************************************************//
+//************************************************************//
+
+#include "texture.h"
+#include "figuras.h"
+#include "Camera.h"
+
+CCamera objCamera;  //Creramos un objeto de tipo camara
+GLfloat g_lookupdown = 0.0f; 
+int font = (int)GLUT_BITMAP_HELVETICA_18;
+
+GLfloat Diffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f };				// Diffuse Light Values
+GLfloat Specular[] = { 1.0, 1.0, 1.0, 1.0 };				// Specular Light Values
+GLfloat Position[] = { 0.0f, 7.0f, -5.0f, 0.0f };			// Light Position
+GLfloat Position2[] = { 0.0f, 0.0f, -5.0f, 1.0f };			// Light Position
+
+CTexture text1;
+CTexture text2;
+
+CFiguras fig1;
+
+void InitGL(GLvoid) {
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				// Negro de fondo	
+	glEnable(GL_TEXTURE_2D);
+	glShadeModel(GL_SMOOTH);
+
+	glLightfv(GL_LIGHT1, GL_POSITION, Position);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, Diffuse);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	glEnable(GL_COLOR_MATERIAL);
+
+	glClearDepth(1.0f);									// Configuramos Depth Buffer
+	glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
+	glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+
+	text1.LoadBMP("cielo.bmp");
+	text1.BuildGLTexture();
+	text1.ReleaseImage();
+	
+	text2.LoadTGA("textura/grass.tga");
+	text2.BuildGLTexture();
+	text2.ReleaseImage();
+
+	objCamera.Position_Camera(0, 2.5f, 3, 0, 2.5f, 0, 0, 1, 0);
+}
+
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	glPushMatrix();
+		glRotatef(g_lookupdown, 1.0f, 0, 0);
+
+		gluLookAt(objCamera.mPos.x, objCamera.mPos.y, objCamera.mPos.z,
+			objCamera.mView.x, objCamera.mView.y, objCamera.mView.z,
+			objCamera.mUp.x, objCamera.mUp.y, objCamera.mUp.z);
+
+		glPushMatrix(); //Creamos cielo y suelo
+			glDisable(GL_LIGHTING);
+			glTranslatef(0, 60, 0);
+			fig1.skybox(130.0, 130.0, 130.0, text1.GLindex, text2.GLindex);
+			glEnable(GL_LIGHTING);
+		glPopMatrix();
+	glPopMatrix();
+
+	glutSwapBuffers();
+}
+
+void reshape(int width, int height)   // Creamos funcion Reshape
+{
+	if (height == 0){ //Prevenimos division por cero
+		height = 1;
+	}
+
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);						// Seleccionamos Projection Matrix
+	glLoadIdentity();
+
+	// Tipo de Vista
+	glFrustum(-0.1, 0.1, -0.1, 0.1, 0.1, 170.0);
+	glMatrixMode(GL_MODELVIEW);							// Seleccionamos Modelview Matrix
+	glLoadIdentity();
+}
+
+void keyboard(unsigned char key, int x, int y)  // Create Keyboard Function
+{
+	switch (key) {
+		case 'w':   //Movimientos de camara
+		case 'W':
+			objCamera.Move_Camera(CAMERASPEED + 0.2);
+			break;
+		case 's':
+		case 'S':
+			objCamera.Move_Camera(-(CAMERASPEED + 0.2));
+			break;
+		case 'a':
+		case 'A':
+			objCamera.Strafe_Camera(-(CAMERASPEED + 0.4));
+			break;
+		case 'd':
+		case 'D':
+			objCamera.Strafe_Camera(CAMERASPEED + 0.4);
+			break;
+		case 27:        // Cuando Esc es presionado...
+			exit(0);   // Salimos del programa
+			break;
+		default:        // Cualquier otra
+			break;
+	}
+	glutPostRedisplay();
+}
+
+void arrow_keys(int a_keys, int x, int y)  // Funcion para manejo de teclas especiales (arrow keys)
+{
+	switch (a_keys) {
+		case GLUT_KEY_PAGE_UP:
+			objCamera.UpDown_Camera(CAMERASPEED);
+			break;
+		case GLUT_KEY_PAGE_DOWN:
+			objCamera.UpDown_Camera(-CAMERASPEED);
+			break;
+		case GLUT_KEY_UP:     // Presionamos tecla ARRIBA...
+			g_lookupdown -= 1.0f;
+			break;
+		case GLUT_KEY_DOWN:               // Presionamos tecla ABAJO...
+			g_lookupdown += 1.0f;
+			break;
+		case GLUT_KEY_LEFT:
+			objCamera.Rotate_View(-CAMERASPEED);
+			break;
+		case GLUT_KEY_RIGHT:
+			objCamera.Rotate_View(CAMERASPEED);
+			break;
+		default:
+			break;
+	}
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv)   // Main Function
+{
+
+	glutInit(&argc, argv); // Inicializamos OpenGL
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // Display Mode (Clores RGB y alpha | Buffer Doble )
+	glutInitWindowSize(500, 500);	// Tama�o de la Ventana
+	glutInitWindowPosition(0, 0);	//Posicion de la Ventana
+	glutCreateWindow("PFCG"); // Nombre de la Ventana
+	//glutFullScreen     ( );         // Full Screen
+	InitGL();						// Parametros iniciales de la aplicacion
+	glutDisplayFunc(display);  //Indicamos a Glut funci�n de dibujo
+	glutReshapeFunc(reshape);	//Indicamos a Glut funci�n en caso de cambio de tamano
+	glutKeyboardFunc(keyboard);	//Indicamos a Glut funci�n de manejo de teclado
+	glutSpecialFunc(arrow_keys);	//Otras
+
+	glutMainLoop();          // 
+
+	return 0;
+}
